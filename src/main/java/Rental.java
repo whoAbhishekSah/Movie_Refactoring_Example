748d86740527055b843c1c47ba4b134b614ade53
Abhishek Sah <abhishek.sah@go-jek.com>
2019-07-29 20:59:54 +0530

    [Abhishek] Formats indentation

--- src/main/java/Rental.java
+++ src/main/java/Rental.java
@@ -2,7 +2,8 @@ class Rental {
     private Movie _movie;
     private int _daysRented;
 
-    public Rental(Movie movie, int daysRented) { _movie = movie;
+    public Rental(Movie movie, int daysRented) {
+        _movie = movie;
         _daysRented = daysRented;
     }
 
