4c386179d7d2b3de5df7e579e873fc109179c578
Abhishek Sah <abhishek.sah@go-jek.com>
2019-07-29 20:50:04 +0530

    [Abhishek] Formats Movie class

--- src/main/java/Movie.java
+++ src/main/java/Movie.java
@@ -2,14 +2,23 @@ public class Movie {
     public static final int CHILDRENS = 2;
     public static final int REGULAR = 0;
     public static final int NEW_RELEASE = 1;
+    private String _title;
+    private int _priceCode;
 
-    private String _title; private int _priceCode;
-    public Movie(String title, int priceCode) { _title = title;
-        _priceCode = priceCode; }
-    public int getPriceCode() { return _priceCode;
+    public Movie(String title, int priceCode) {
+        _title = title;
+        _priceCode = priceCode;
     }
-    public void setPriceCode(int arg) { _priceCode = arg;
+
+    public int getPriceCode() {
+        return _priceCode;
     }
-    public String getTitle (){ return _title;
+
+    public void setPriceCode(int arg) {
+        _priceCode = arg;
+    }
+
+    public String getTitle (){
+        return _title;
     };
 }
