f3d2fc2c20926ea04dd8f690881084481b9face8
Abhishek Sah <abhishek.sah@go-jek.com>
2019-07-30 20:33:57 +0530

    [Abhishek] Makes getCharge method abstract and implements in child classes

--- src/main/java/NewReleasePrice.java
+++ src/main/java/NewReleasePrice.java
@@ -2,4 +2,8 @@ class NewReleasePrice extends Price {
     int getPriceCode() {
         return Movie.NEW_RELEASE;
     }
+
+    double getCharge(int daysRented){
+        return daysRented * 3;
+    }
 }
--- src/main/java/RegularPrice.java
+++ src/main/java/RegularPrice.java
@@ -2,4 +2,11 @@ class RegularPrice extends Price {
     int getPriceCode() {
         return Movie.REGULAR;
     }
+
+    double getCharge(int daysRented){
+        double result = 2;
+        if (daysRented > 2)
+            result += (daysRented - 2) * 1.5;
+        return result;
+    }
 }
