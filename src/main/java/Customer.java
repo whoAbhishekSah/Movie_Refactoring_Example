2bc73c47bda5b0244695db3d3379972dfb1f5327
Abhishek Sah <abhishek.sah@go-jek.com>
2019-07-30 19:21:03 +0530

    [Abhishek] Adds method to generate html statement

--- src/main/java/Customer.java
+++ src/main/java/Customer.java
@@ -50,4 +50,16 @@ class Customer {
             result += each.getFrequentRenterPoints(); }
         return result;
     }
+
+    public String htmlStatement() {
+        Enumeration rentals = _rentals.elements();
+        String result = "<H1>Rentals for <EM>" + getName() + "</EM></ H1><P>\n";
+        while (rentals.hasMoreElements()) {
+            Rental each = (Rental) rentals.nextElement();
+            result += each.getMovie().getTitle() + ":" + String.valueOf(each.getCharge()) + "<BR>\n";
+        }
+        result += "<P>You owe <EM>" + String.valueOf(getTotalCharge()) + "</EM><P>\n";
+        result += "On this rental you earned <EM>" + String.valueOf(getTotalFrequentRenterPoints()) + "</EM> frequent renter points<P>";
+        return result;
+    }
 }
