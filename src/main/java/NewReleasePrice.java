704d5e75b9173a45afe2cdfd63f4a5c608ca0f6f
Abhishek Sah <abhishek.sah@go-jek.com>
2019-07-30 20:38:11 +0530

    [Abhishek] Overrides method in new movie release class

--- src/main/java/NewReleasePrice.java
+++ src/main/java/NewReleasePrice.java
@@ -6,4 +6,8 @@ class NewReleasePrice extends Price {
     double getCharge(int daysRented){
         return daysRented * 3;
     }
+
+    int getFrequentRenterPoints(int daysRented) {
+        return (daysRented > 1) ? 2: 1;
+    }
 }
--- src/main/java/Price.java
+++ src/main/java/Price.java
@@ -4,9 +4,6 @@ abstract class Price {
     abstract double getCharge(int daysRented);
 
     int getFrequentRenterPoints(int daysRented) {
-        if ((getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1)
-            return 2;
-        else
-            return 1;
+        return 1;
     }
 }
