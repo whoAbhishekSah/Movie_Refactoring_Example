d464430f65aeb036ca4c863b40623dbba58d43a7
Abhishek Sah <abhishek.sah@go-jek.com>
2019-07-30 20:14:46 +0530

    [Abhishek] Moves renter pointe calculation method to Movie class

--- src/main/java/Rental.java
+++ src/main/java/Rental.java
@@ -20,10 +20,7 @@ class Rental {
     }
 
     public int getFrequentRenterPoints(){
-        if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) && getDaysRented() > 1)
-            return 2;
-        else
-            return 1;
+        return _movie.getFrequentRenterPoints(_daysRented);
 
     }
 }
