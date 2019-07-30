e33486544f25466fd89e853af18c5661e9f6a6c6
Abhishek Sah <abhishek.sah@go-jek.com>
2019-07-30 20:36:34 +0530

    [Abhishek] Adds getFrequentRenterPoints to Price class

--- src/main/java/Price.java
+++ src/main/java/Price.java
@@ -2,4 +2,11 @@ abstract class Price {
     abstract int getPriceCode();
 
     abstract double getCharge(int daysRented);
+
+    int getFrequentRenterPoints(int daysRented) {
+        if ((getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1)
+            return 2;
+        else
+            return 1;
+    }
 }
